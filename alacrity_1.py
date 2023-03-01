57c0ec5dd20a574722287176b447448ed5366a79
Sauronil Das <sauronildas10@gmail.com>
2023-03-01 16:40:12 +0000

    edits by Nathan(Alacrity)

--- alacrity_1.py
+++ alacrity_1.py
@@ -8,11 +8,13 @@ def user_interface():
     user_input_1=input("Do you want to find a prime number or a range of prime numbers?\nPlease enter: Y for prime numbers || N for range of prime numbers: ")
     if user_input_1 in ["Y", "y", "yes"]:
         user_input_2=input("Enter Integer: ")
-        get_factors(int(user_input_2))
+        handle_input(int(user_input_2))
+
     elif user_input_1 in ["N", "n", "no"]:
         user_input_2=input("Enter Initial Value: ")
         user_input_3=input("Enter Last Value: ")
-        find_primes(int(user_input_2), int(user_input_3))
+        primes = find_primes(int(user_input_2), int(user_input_3))
+        print(primes)
     else:
         print("Invalid input")
         sleep(1)
@@ -21,40 +23,33 @@ def user_interface():
 
 
 def is_prime(x):
-    # Check if given number is prime and return True if it is, False otherwise
-    if x > 1:
-    # Since prime starts after 1 (unit), 0 is compopsite
-        for n in range(2, x):
-            if (x%n) == 0:
-                #checks if number is divisible
-                return False
-        return True
-    else:
-        return False
+    return len(get_factors(x, True)) == 0
 
-def set_factors(x):
+def get_factors(x, stopOnFirst = False):
     #This function finds the factors of a given number
     factors = []
-    for i in range(1, x+1):
+    for i in range(2, x):
         if x % i == 0:
             factors.append(i)
+            if stopOnFirst:
+                break
     return factors
-def get_factors(n):
+
+def handle_input(n):
     #finds the factors of a number if it is not prime.
     if is_prime(n):
         print(f"{n} is a prime number.")
     else:
-        factors = factors(n)
+        factors = get_factors(n)
         print(f"{n} is not a prime number. Its factors are: {factors}")
 
+# ex oflast
 def find_primes(first, last):
     #function to finds all prime numbers in the given range and their factors
     primes = []
-    for i in range(first, last+1):
+    for i in range(first, last):
         if is_prime(i):
             primes.append(i)
-            factors = set_factors(i)
-            print(f"{i} is a prime number. Its factors are: {factors}")
     return primes
     
 user_interface()
